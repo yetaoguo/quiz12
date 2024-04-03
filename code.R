99a82460a9c65d75562e7f7779cc0c11ed586d6d
Yetao Guo <yetaoguo0315@gmail.com>
2024-04-03 03:30:22 +0000

    add

--- code.R
+++ code.R
@@ -1,3 +1,34 @@
+
+Q2.
+library(tidyverse)
+
+n_years <- 20
+n_hospitals <- 5
+n_cancer_types <- 4
+n_age_groups <- 5
+n_records <- 1000  
+
+
+set.seed(123)  
+data <- tibble(
+  Hospital = rep(paste("Hospital", LETTERS[1:n_hospitals]), length.out = n_records),
+  Year = sample(2004:2023, size = n_records, replace = TRUE),
+  CancerType = sample(c("Lung", "Breast", "Prostate", "Colorectal"), size = n_records, replace = TRUE),
+  Deaths = sample(50:200, size = n_records, replace = TRUE),
+  AgeGroup = sample(c("0-18", "19-30", "31-45", "46-60", "61+"), size = n_records, replace = TRUE),
+  Gender = sample(c("Male", "Female"), size = n_records, replace = TRUE)
+)
+
+
+head(data)
+
+
+
+
+
+
+Q4:
+
 install.packages("ggplot2")
 install.packages("rstanarm")
 
